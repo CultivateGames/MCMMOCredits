0aecb90dd5bd1512774e5d3aff7dd7e4ba6dc690
NolanMC <nolanminecraftserver@gmail.com>
2022-01-04 12:07:47 -0600

    Add creload alias, and additional file reload options.

--- src/main/java/games/cultivate/mcmmocredits/commands/Reload.java
+++ src/main/java/games/cultivate/mcmmocredits/commands/Reload.java
@@ -11,22 +11,50 @@ import org.bukkit.entity.Player;
  * <p>This class houses one command method, which is for reloading plugin files.</p>
  * <p>Due to the way Configurate works, all we have to do is call the same methods used on plugin startup.</p>
  *
- * @see Reload#reloadFiles(CommandSender)
+ * @see Reload#reloadAll(CommandSender)
  * @see <a href="https://github.com/SpongePowered/Configurate" target="_top">Thank you to Configurate!</a>
  */
+@CommandMethod("creditsreload|creload")
 public class Reload {
     /**
-     * <p>This is a command that is used to reload both settings.conf and messages.conf if a change was made.
-     * This should be reserved for use by administrators. If the command is executed by Console, we skip placeholder parsing.</p>
+     * <p>Command used to reload the settings.conf file.</p>
      *
      * @param sender The {@link CommandSender} that executed the Command.
      */
-    @CommandDescription("MCMMO Credits Reload")
-    @CommandMethod("creditsreload")
-    @CommandPermission("mcmmocredits.admin")
-    private void reloadFiles(CommandSender sender) {
+    @CommandDescription("MCMMO Credits Reload - Settings")
+    @CommandMethod("settings")
+    @CommandPermission("mcmmocredits.admin.reload.settings")
+    private void reloadSettings(CommandSender sender) {
+        ConfigHandler.loadFile("settings");
+        this.sendReloadMessage(sender);
+    }
+    /**
+     * <p>Command used to reload the messages.conf file.</p>
+     *
+     * @param sender The {@link CommandSender} that executed the Command.
+     */
+    @CommandDescription("MCMMO Credits Reload - Settings")
+    @CommandMethod("messages")
+    @CommandPermission("mcmmocredits.admin.reload.messages")
+    private void reloadMessages(CommandSender sender) {
+        ConfigHandler.loadFile("messages");
+        this.sendReloadMessage(sender);
+    }
+    /**
+     * <p>Command used to reload all configuration files.</p>
+     *
+     * @param sender The {@link CommandSender} that executed the Command.
+     */
+    @CommandDescription("MCMMO Credits Reload - All Files")
+    @CommandMethod("all")
+    @CommandPermission("mcmmocredits.admin.reload")
+    private void reloadAll(CommandSender sender) {
         ConfigHandler.loadFile("settings");
         ConfigHandler.loadFile("messages");
+        this.sendReloadMessage(sender);
+    }
+
+    private void sendReloadMessage(CommandSender sender) {
         if (sender instanceof Player) {
             ConfigHandler.sendMessage(sender, ConfigHandler.parse((Player) sender, ConfigHandler.message("reload-successful")));
         } else {
