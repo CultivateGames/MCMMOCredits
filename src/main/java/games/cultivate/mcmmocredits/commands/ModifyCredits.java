b2e322beebada92eb20a73e20ff4ea411cbaf4de
NolanMC <nolanminecraftserver@gmail.com>
2022-01-21 17:34:14 -0600

    refactor configuration a whole lot more, and introduce transaction pairs

--- src/main/java/games/cultivate/mcmmocredits/commands/ModifyCredits.java
+++ src/main/java/games/cultivate/mcmmocredits/commands/ModifyCredits.java
@@ -10,6 +10,7 @@ import games.cultivate.mcmmocredits.config.ConfigHandler;
 import games.cultivate.mcmmocredits.config.Keys;
 import games.cultivate.mcmmocredits.database.Database;
 import games.cultivate.mcmmocredits.util.Util;
+import it.unimi.dsi.fastutil.Pair;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -18,34 +19,45 @@ import org.bukkit.entity.Player;
  */
 @CommandMethod("modifycredits")
 public class ModifyCredits {
-
     @CommandDescription("Add MCMMO Credits to a user's balance.")
-    @CommandMethod("add <amount> <player> <silent>")
+    @CommandMethod("add <amount> <player>")
     @CommandPermission("mcmmocredits.admin.modify")
-    private void addCredits(CommandSender sender, @Argument("amount") @Range(min = "1", max = "2147483647") int amount, @Argument("player") Player player, @Flag(value = "silent", permission = "mcmmocredits.admin.modify.silent") boolean silent) {
+    private void addCredits(CommandSender sender, @Argument("amount") @Range(min = "1", max = "2147483647") int amount, @Argument("player") Player player, @Flag("silent") boolean silent) {
         if (Util.shouldProcess(sender, player)) {
             Database.addCredits(Util.getUser(player), amount);
-            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_ADD.getString(), Util.transactionalBuilder(player, amount).build());
+            Pair<CommandSender, Player> transactionPair = Pair.of(sender, player);
+            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_ADD_SENDER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            if (!silent) {
+                ConfigHandler.sendMessage(player, Keys.MODIFY_CREDITS_ADD_RECEIVER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            }
         }
     }
 
     @CommandDescription("Set a user's MCMMO Credit balance to the specified amount.")
-    @CommandMethod("set <amount> <player> <silent>")
+    @CommandMethod("set <amount> <player>")
     @CommandPermission("mcmmocredits.admin.modify")
     private void setCredits(CommandSender sender, @Argument("amount") @Range(min = "0", max = "2147483647") int amount, @Argument("player") Player player, @Flag(value = "silent", permission = "mcmmocredits.admin.modify.silent") boolean silent) {
         if (Util.shouldProcess(sender, player)) {
             Database.setCredits(Util.getUser(player), amount);
-            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_SET.getString(), Util.transactionalBuilder(player, amount).build());
+            Pair<CommandSender, Player> transactionPair = Pair.of(sender, player);
+            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_SET_SENDER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            if (!silent) {
+                ConfigHandler.sendMessage(player, Keys.MODIFY_CREDITS_SET_RECEIVER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            }
         }
     }
 
     @CommandDescription("Take MCMMO Credits away from a user's balance")
-    @CommandMethod("take <amount> <player> <silent>")
+    @CommandMethod("take <amount> <player>")
     @CommandPermission("mcmmocredits.admin.modify")
     private void takeCredits(CommandSender sender, @Argument("amount") @Range(min = "1", max = "2147483647") int amount, @Argument("player") Player player, @Flag(value = "silent", permission = "mcmmocredits.admin.modify.silent") boolean silent) {
         if (Util.shouldProcess(sender, player)) {
             Database.takeCredits(Util.getUser(player), amount);
-            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_TAKE.getString(), Util.transactionalBuilder(player, amount).build());
+            Pair<CommandSender, Player> transactionPair = Pair.of(sender, player);
+            ConfigHandler.sendMessage(sender, Keys.MODIFY_CREDITS_TAKE_SENDER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            if (!silent) {
+                ConfigHandler.sendMessage(player, Keys.MODIFY_CREDITS_TAKE_RECEIVER.getString(), Util.transactionBuilder(transactionPair, amount).build());
+            }
         }
     }
 }
