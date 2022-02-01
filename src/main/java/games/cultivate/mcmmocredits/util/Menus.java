788e6554203d01473db7a9d8fe8d8e0177ab83fa
NolanMC <nolanminecraftserver@gmail.com>
2022-02-01 10:32:05 -0600

    navigation and small changes

--- src/main/java/games/cultivate/mcmmocredits/util/Menus.java
+++ src/main/java/games/cultivate/mcmmocredits/util/Menus.java
@@ -7,8 +7,6 @@ import it.unimi.dsi.fastutil.Pair;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.Style;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.entity.Player;
@@ -23,17 +21,11 @@ import org.incendo.interfaces.paper.utils.PaperUtils;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class Menus {
-    private static final Style defaultStyle = Style.style().decoration(TextDecoration.ITALIC, false).color(NamedTextColor.GRAY).build();
     public static Map<UUID, CompletableFuture<String>> inputMap = new HashMap<>();
-
-    protected static Pair<ItemStack, Vector2> prepareItem(ItemStack item) {
-        return Pair.of(item, PaperUtils.slotToGrid(item.getItemMeta().getPersistentDataContainer().getOrDefault(MCMMOCredits.key, PersistentDataType.INTEGER, 0)));
-    }
 
     public static ChestInterface.Builder constructInterface(Player player, Keys title, Keys slots) {
         ChestInterface.Builder cb = ChestInterface.builder()
@@ -69,14 +61,30 @@ public class Menus {
      * public static ChestInterface.Builder redeemMenu(Player player) {}
      */
 
-    public static void mainMenu(Player player) {
-        ChestInterface.Builder cb = constructInterface(player, Keys.MENU_TITLE, Keys.MENU_SIZE);
+    public static void openMainMenu(Player player) {
+        constructMainMenu(player, Keys.MENU_TITLE, Keys.MENU_SIZE).open(PlayerViewer.of(player));
+    }
+
+    public static void openMessagesMenu(Player player) {
+        constructConfigInterface(player, Keys.EDIT_MESSAGES_ITEM, Keys.EDIT_MESSAGES_TITLE, Keys.EDIT_MESSAGES_SIZE).open(PlayerViewer.of(player));
+    }
+
+    public static void openSettingsMenu(Player player) {
+        constructConfigInterface(player, Keys.EDIT_SETTINGS_ITEM, Keys.EDIT_MESSAGES_TITLE, Keys.EDIT_MESSAGES_SIZE).open(PlayerViewer.of(player));
+    }
+
+    public static ChestInterface constructMainMenu(Player player, Keys title, Keys rows) {
+        ChestInterface.Builder cb = constructInterface(player, title, rows);
         cb = basicTransform(cb, prepareItem(Keys.MENU_REDEEM_ITEM.getItemStack(player)));
         if (player.hasPermission("mcmmocredits.gui.admin")) {
             cb = transferLeftClick(cb, constructConfigInterface(player, Keys.EDIT_MESSAGES_ITEM, Keys.EDIT_MESSAGES_TITLE, Keys.EDIT_MESSAGES_SIZE), prepareItem(Keys.MENU_MESSAGES_ITEM.getItemStack(player)));
             cb = transferLeftClick(cb, constructConfigInterface(player, Keys.EDIT_SETTINGS_ITEM, Keys.EDIT_SETTINGS_TITLE, Keys.EDIT_SETTINGS_SIZE), prepareItem(Keys.MENU_SETTINGS_ITEM.getItemStack(player)));
         }
-        cb.build().open(PlayerViewer.of(player));
+        return cb.build();
+    }
+
+    private static void sendConfigMessage(Player player, String string) {
+        player.sendMessage(Component.newline().toBuilder().append(Component.text("Enter a new value for: ").color(NamedTextColor.RED), Component.text(string, Util.defaultStyle), Component.text(" in chat."), Component.newline()));
     }
 
     public static ChestInterface constructConfigInterface(Player player, Keys itemStack, Keys title, Keys rows) {
@@ -85,22 +93,23 @@ public class Menus {
             cb = cb.addTransform(1, (pane, view) -> pane.element(ItemStackElement.of(item.getKey(), (clickHandler) -> {
                 if (clickHandler.click().leftClick()) {
                     view.viewer().close();
-                    String pathString = Objects.requireNonNull(clickHandler.cause().getCurrentItem()).getItemMeta().getPersistentDataContainer().get(MCMMOCredits.key, PersistentDataType.STRING);
-                    if (clickHandler.cause().getWhoClicked() instanceof Player clicker) {
-                        inputMap.put(clicker.getUniqueId(), new CompletableFuture<>());
-                        //TODO extract text out to method.
-                        player.sendMessage(Component.newline());
-                        player.sendMessage(Component.text("Enter a new value for: ").color(NamedTextColor.RED).append(Component.text(Objects.requireNonNull(pathString), defaultStyle).append(Component.text(" in chat."))));
-                        player.sendMessage(Component.newline());
-
-                        //Change Config
-                        UUID uuid = player.getUniqueId();
-                        Menus.inputMap.get(uuid).thenAcceptAsync((i) -> ConfigHandler.changeConfigInGame(StringUtils.split(pathString, "."), i)).whenComplete((i, throwable) -> Menus.inputMap.remove(uuid));
+                    ItemStack stack = clickHandler.cause().getCurrentItem();
+                    if (stack != null && stack.hasItemMeta()) {
+                        String pathString = stack.getItemMeta().getPersistentDataContainer().get(MCMMOCredits.key, PersistentDataType.STRING);
+                        if (clickHandler.cause().getWhoClicked() instanceof Player clicker) {
+                            inputMap.put(clicker.getUniqueId(), new CompletableFuture<>());
+                            sendConfigMessage(player, pathString);
+                            //Change Config
+                            UUID uuid = player.getUniqueId();
+                            Menus.inputMap.get(uuid).thenAcceptAsync((i) -> ConfigHandler.changeConfigInGame(StringUtils.split(pathString, "."), i)).whenComplete((i, throwable) -> Menus.inputMap.remove(uuid));
+                        }
                     }
                 }
             }), item.getValue().x(), item.getValue().getY()));
         }
-        //TODO Navigation
+        if (Keys.MENU_NAVIGATION.getBoolean()) {
+            cb = transferLeftClick(cb, constructMainMenu(player, Keys.MENU_TITLE, Keys.MENU_SIZE), prepareItem(Keys.MENU_NAVIGATION.getItemStack(player)));
+        }
         return cb.build();
     }
 
@@ -112,12 +121,16 @@ public class Menus {
             item.setAmount(slot + 1);
             item.editMeta(meta -> {
                 String path = StringUtils.join(key.path(), ".");
-                meta.displayName(Component.text(path, defaultStyle));
+                meta.displayName(Component.text(path, Util.defaultStyle));
                 meta.getPersistentDataContainer().set(MCMMOCredits.key, PersistentDataType.STRING, path);
             });
             configItems.put(item, PaperUtils.slotToGrid(slot));
             slot++;
         }
         return configItems;
+    }
+
+    protected static Pair<ItemStack, Vector2> prepareItem(ItemStack item) {
+        return Pair.of(item, PaperUtils.slotToGrid(item.getItemMeta().getPersistentDataContainer().getOrDefault(MCMMOCredits.key, PersistentDataType.INTEGER, 0)));
     }
 }
