3a552aca83b2d776074d2bb0a341f2625e91598b
NolanMC <nolanminecraftserver@gmail.com>
2022-08-14 18:12:17 -0500

    Menu fixes, fix redemption via menu

--- src/main/java/games/cultivate/mcmmocredits/menu/MainMenu.java
+++ src/main/java/games/cultivate/mcmmocredits/menu/MainMenu.java
@@ -4,9 +4,9 @@ import games.cultivate.mcmmocredits.config.MenuConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.incendo.interfaces.core.transform.TransformContext;
+import org.incendo.interfaces.paper.PlayerViewer;
 import org.incendo.interfaces.paper.element.ItemStackElement;
-
-import java.util.List;
+import org.incendo.interfaces.paper.pane.ChestPane;
 
 public final class MainMenu extends BaseMenu {
     public MainMenu(final MenuConfig menu, final Player player) {
@@ -15,20 +15,22 @@ public final class MainMenu extends BaseMenu {
 
     @Override
     public void applySpecialItems() {
-        List<String> menus = List.of("messages", "settings", "redeem");
-        this.transformations.add(TransformContext.of(0, (pane, view) -> {
-            for (String type : menus) {
-                if (this.player.hasPermission("mcmmocredits.menu." + type)) {
-                    String menuPath = "main.items." + type;
-                    int slot = this.menu.slot(menuPath);
-                    pane = pane.element(ItemStackElement.of(this.menu.item(menuPath, this.player), click -> {
-                        if (click.cause().isLeftClick()) {
-                            Bukkit.dispatchCommand(this.player, "credits menu " + type);
-                        }
-                    }), slot % 9, slot / 9);
-                }
+        this.transformations.add(this.mainTransformContext("config"));
+        this.transformations.add(this.mainTransformContext("redeem"));
+    }
+
+    private TransformContext<ChestPane, PlayerViewer> mainTransformContext(String type) {
+        return TransformContext.of(0, (pane, view) -> {
+            if (this.player.hasPermission("mcmmocredits.menu." + type)) {
+                String menuPath = "main.items." + type;
+                int slot = this.menu.slot(menuPath);
+                pane = pane.element(ItemStackElement.of(this.menu.item(menuPath, this.player), click -> {
+                    if (click.cause().isLeftClick()) {
+                        Bukkit.dispatchCommand(this.player, "credits menu " + type);
+                    }
+                }), slot % 9, slot / 9);
             }
             return pane;
-        }));
+        });
     }
 }
