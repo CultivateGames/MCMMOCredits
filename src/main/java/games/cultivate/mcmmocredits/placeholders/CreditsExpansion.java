0cee10e666a55c41b8cc2416e6f32380878ffcf6
NFRCG <nolanminecraftserver@gmail.com>
2022-09-03 07:52:10 -0500

    minor adjustments to build/configs, bump version

--- src/main/java/games/cultivate/mcmmocredits/placeholders/CreditsExpansion.java
+++ src/main/java/games/cultivate/mcmmocredits/placeholders/CreditsExpansion.java
@@ -63,7 +63,7 @@ public final class CreditsExpansion extends PlaceholderExpansion {
      */
     @Override
     public @NotNull String getVersion() {
-        return "0.1.2";
+        return "0.2.1";
     }
 
     /**
