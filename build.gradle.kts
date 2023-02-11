6e758b0c79f54d9de68e7f3950fb86bd4179fe7a
NFRCG <nolanminecraftserver@gmail.com>
2023-02-10 22:29:51 -0600

    publication: trying to fix publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,8 +75,6 @@ nexusPublishing {
         create("myNexus") {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(System.getenv("SONATYPE_USERNAME"))
-            password.set(System.getenv("SONATYPE_PASSWORD"))
         }
     }
 }
